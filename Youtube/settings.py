72a2e635f902ea7eaa133e394255fe2748134137
Vandhana05 <99535347+Vandhana05@users.noreply.github.com>
2024-01-18 17:11:09 +0530

    Update settings.py
    chnage host to all

--- Youtube/settings.py
+++ Youtube/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-%tcb1rvlow!(5g6i)*(#yf&g0&@%cpuah78!!1nu=p8820bycs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
