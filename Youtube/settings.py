b14d3c2f9b56f9ed92163f8817f2c7e307ae3607
Vandhana05 <99535347+Vandhana05@users.noreply.github.com>
2024-01-18 17:12:36 +0530

    Update settings.py

--- Youtube/settings.py
+++ Youtube/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%tcb1rvlow!(5g6i)*(#yf&g0&@%cpuah78!!1nu=p8820bycs"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
